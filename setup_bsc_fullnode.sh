9038dba9e691a2a6dbe2bb8921cdc5686e1b6b8c
GalaIO <GalaIO@users.noreply.github.com>
2024-03-18 11:21:32 +0800

    scripts: add new fullnode helper script;

--- setup_bsc_fullnode.sh
+++ setup_bsc_fullnode.sh
@@ -79,7 +79,7 @@ clean)
     clean
     ;;
 *)
-    echo "Usage: run_fullnode.sh start|stop|clean nodeIndex syncmode"
-    echo "like: run_fullnode.sh start 1 snap, it will startup a snapsync node1"
+    echo "Usage: setup_bsc_fullnode.sh start|stop|clean nodeIndex syncmode"
+    echo "like: setup_bsc_fullnode.sh start 1 snap, it will startup a snapsync node1"
     ;;
 esac
