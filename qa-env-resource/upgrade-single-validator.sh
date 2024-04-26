228a594977768e4d5c4b587d7a0d0ce611c5fb57
NathanBSC <Nathan.l@nodereal.io>
2024-04-26 15:52:27 +0800

    support QA deploy

---
+++ qa-env-resource/upgrade-single-validator.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+service bsc stop
+cp /tmp/geth /server/validator/bsc && chmod u+x /server/validator/bsc
+# update `config` in genesis.json
+# /server/validator/bsc dumpgenesis  --datadir /server/validator/ > /server/validator/genesis.json
+
+#cd  /server/validator/geth/
+#rm -rf  chaindata  les.server  nodes  triecache
+
+#sed -i -e 's/sleep 5/sleep 40/' /server/validator/chaind.sh
+
+#/server/validator/bsc init --datadir /server/validator/ /server/validator/genesis.json
+
+#sed -i -e 's/HTTPModules = \[/HTTPModules = \["debug",/g' /server/validator/config.toml
+#sed -i -e 's/GlobalSlots = 10000/GlobalSlots = 10000/g' /server/validator/config.toml
+#sed -i -e 's/GlobalQueue = 5000/GlobalQueue = 5000/g' /server/validator/config.toml
+#sed -i -e 's/Level = "info"/Level = "debug"/g' /server/validator/config.toml
+#sed -i -e 's/GasCeil = 30000000/GasCeil = 60000000/g' /server/validator/config.toml
+
+#sed -i 's/18000000/1800/g'  /server/validator/chaind.sh
+#sed -i 's/ --override.pascal 1747481702  --override.prague 1747481702 /  /g'  /server/validator/chaind.sh
+#sed -i 's/--override.lorentz / --override.pascal 1748515525  --override.prague 1748515525 --override.lorentz /g'  /server/validator/chaind.sh
+
+service bsc start
