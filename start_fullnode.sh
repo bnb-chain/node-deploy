94427f4cf9b66502759c853d745dfefe47e5e25d
NathanBSC <Nathan.l@nodereal.io>
2024-07-10 10:40:26 +0800

    sync start_fullnode.sh with start_cluster.sh about the harfork flags

--- start_fullnode.sh
+++ start_fullnode.sh
@@ -36,8 +36,7 @@ node=node$index
 dst=${workspace}/.local/bsc/fullnode/${node}
 hardforkfile=${workspace}/.local/bsc/hardforkTime.txt
 rialtoHash=`cat $src/init.log|grep "database=lightchaindata"|awk -F"=" '{print $NF}'|awk -F'"' '{print $1}'`
-FeynmanHardforkTime=`cat $hardforkfile | grep hardforkTime | awk -F" " '{print $NF}'`
-CancunHardforkTime=`expr ${FeynmanHardforkTime} + 10`
+BohrHardforkTime=`cat $hardforkfile | grep hardforkTime | awk -F" " '{print $NF}'`
 
 mkdir -pv $dst/
 
@@ -52,7 +51,7 @@ function start() {
   --ws.addr 0.0.0.0 --ws.port $(( 8600 + $index )) --http.addr 0.0.0.0 --http.port $(( 8600 + $index )) --http.corsdomain "*" \
   --metrics --metrics.addr 0.0.0.0 --metrics.port $(( 6100 + $index )) --metrics.expensive \
   --gcmode $gcmode --syncmode $syncmode --state.scheme ${stateScheme} $extraflags \
-  --rialtohash ${rialtoHash} --override.feynman ${FeynmanHardforkTime} --override.feynmanfix ${FeynmanHardforkTime} --override.cancun ${CancunHardforkTime} \
+  --rialtohash ${rialtoHash} --override.bohr ${BohrHardforkTime} \
   --override.immutabilitythreshold ${FullImmutabilityThreshold} --override.minforblobrequest ${MinBlocksForBlobRequests} --override.defaultextrareserve ${DefaultExtraReserveForBlobRequests} \
   > $dst/bsc-node.log 2>&1 &
   echo $! > $dst/pid
