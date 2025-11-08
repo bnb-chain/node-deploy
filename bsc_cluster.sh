dfcd5673904386a88cce4eca8925c5931a0fca33
GalaIO <GalaIO@users.noreply.github.com>
2025-11-08 17:02:16 +0800

    config: opt setup script;

--- bsc_cluster.sh
+++ bsc_cluster.sh
@@ -341,7 +341,7 @@ function start_reth_bsc() {
     echo "node${nodeIndex}, nodekey_path: ${nodekey_path}, peer_conf: ${peer_conf[@]}, evn_conf: ${evn_conf[@]}"
     
     # Run reth-bsc node
-    nohup env RUST_LOG=trace BREATHE_BLOCK_INTERVAL=${BreatheBlockInterval} ${RETH_BSC_BINARY_PATH} node \
+    nohup env RUST_LOG=debug BREATHE_BLOCK_INTERVAL=${BreatheBlockInterval} ${RETH_BSC_BINARY_PATH} node \
         --chain ${workspace}/.local/node${nodeIndex}/genesis_reth.json \
         --datadir ${workspace}/.local/node${nodeIndex} \
         --genesis-hash ${rialtoHash} \
@@ -389,7 +389,7 @@ function start_reth_bsc() {
         fi
 
         echo "sentry${nodeIndex}, nodekey_path: ${nodekey_path}, peer_conf: ${peer_conf[@]}, evn_conf: ${evn_conf[@]}"
-        nohup env RUST_LOG=trace BREATHE_BLOCK_INTERVAL=${BreatheBlockInterval} ${RETH_BSC_BINARY_PATH} node \
+        nohup env RUST_LOG=debug BREATHE_BLOCK_INTERVAL=${BreatheBlockInterval} ${RETH_BSC_BINARY_PATH} node \
             --chain ${workspace}/.local/sentry${nodeIndex}/genesis_reth.json \
             --datadir ${workspace}/.local/sentry${nodeIndex} \
             --genesis-hash ${rialtoHash} \
